4a378fe4cad6c305cdaee5c94241ddd7c6ab3b99
Westly David <westlydavid.f@blp.co.in>
2025-09-17 14:52:13 +0530

    init

---
+++ src/app/church-admin/church-admin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ChurchAdminRoutingModule } from './church-admin-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ChurchAdminRoutingModule
+  ]
+})
+export class ChurchAdminModule { }
